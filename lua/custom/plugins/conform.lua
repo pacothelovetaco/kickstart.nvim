633f9672ae0a18794f61dcd44265a37baafe38b1
Justin Leavitt <buzygamerdad@gmail.com>
2025-02-17 11:06:24 -0800

    My edits

---
+++ lua/custom/plugins/conform.lua
@@ -0,0 +1,29 @@
+-- File: lua/custom/plugins/confrom.lua
+
+return {
+  "stevearc/conform.nvim",
+  event = { "BufWritePre" },
+  cmd = { "ConformInfo" },
+  keys = {
+    {
+      -- Customize or remove this keymap to your liking
+      "<leader>f",
+      function()
+        require("conform").format({ async = true, lsp_fallback = true })
+      end,
+      mode = "",
+      desc = "Format buffer",
+    },
+  },
+  -- Everything in opts will be passed to setup()
+  opts = {
+    -- Define your formatters
+    formatters_by_ft = {
+      lua = { "stylua" },
+      javascript = { { "prettierd", "prettier" } },
+    },
+    -- Set up format-on-save
+    -- format_on_save = { timeout_ms = 500, lsp_fallback = true },
+  },
+}
+
