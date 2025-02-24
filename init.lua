515854d6a313fd7aa8a08bea374f4e01ccb07245
Justin Leavitt <buzygamerdad@gmail.com>
2025-02-24 10:50:03 -0800

    Upate to the latest in main

--- init.lua
+++ init.lua
@@ -670,6 +670,9 @@ require('lazy').setup({
         -- But for many setups, the LSP (`ts_ls`) will work just fine
         -- ts_ls = {},
         --
+        ts_ls = {},
+        html = { filetypes = { 'html', 'twig', 'hbs' } },
+        eslint = { nodePath = '~/.nvm/versions/node/v22.2.0/lib/node_modules/' },
 
         lua_ls = {
           -- cmd = { ... },
@@ -992,7 +995,7 @@ require('lazy').setup({
   --    This is the easiest way to modularize your config.
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-  -- { import = 'custom.plugins' },
+  { import = 'custom.plugins' },
   --
   -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
   -- Or use telescope!
--- lua/custom/plugins/conform.lua
+++ lua/custom/plugins/conform.lua
@@ -1,29 +1,28 @@
 -- File: lua/custom/plugins/confrom.lua
 
 return {
-  "stevearc/conform.nvim",
-  event = { "BufWritePre" },
-  cmd = { "ConformInfo" },
+  'stevearc/conform.nvim',
+  event = { 'BufWritePre' },
+  cmd = { 'ConformInfo' },
   keys = {
     {
       -- Customize or remove this keymap to your liking
-      "<leader>f",
+      '<leader>f',
       function()
-        require("conform").format({ async = true, lsp_fallback = true })
+        require('conform').format { async = true, lsp_fallback = true }
       end,
-      mode = "",
-      desc = "Format buffer",
+      mode = '',
+      desc = 'Format buffer',
     },
   },
   -- Everything in opts will be passed to setup()
   opts = {
     -- Define your formatters
     formatters_by_ft = {
-      lua = { "stylua" },
-      javascript = { { "prettierd", "prettier" } },
+      lua = { 'stylua' },
+      javascript = { 'prettierd', 'prettier' },
     },
     -- Set up format-on-save
     -- format_on_save = { timeout_ms = 500, lsp_fallback = true },
   },
 }
-
